324b73f87170c6338d311afb3291fdd48c8136a4
John Christensen <johnawesomejr@gmail.com>
2022-06-14 22:38:12 -0500

    added variables to fix an error

--- buildArray.php
+++ buildArray.php
@@ -1,51 +1,12 @@
 <?php
 $projectName = 'testLaravel';
-$globalName = 'hotel';
+$globalName = 'contributer';
 $globalMaxNumberPerPage = 2;
 $globalColumns = array(
     [
         'type' => 'dbRow',
         'name' => 'name',
         'htmlInputType' => 'text'
-    ],
-    [
-        'type' => 'dbRow',
-        'name' => 'location',
-        'htmlInputType' => 'text'
-    ],
-    [
-        'type' => 'selectList',
-        'name' => 'Other',
-        'htmlInputType' => 'select',
-        'databaseName' => 'party',
-        'columnShown' => 'location',
-        'IdCollumnForThisTable' => 'party_id',
-        'ForenIdColumn' => 'id'
-    ],
-    [
-        'type' => 'oneToMany',
-        'name' => 'comments',
-        'databaseName' => 'comments',
-        'IdCollumnForThisTable' => 'id',
-        'ForenIdColumn' => 'commentParent',
-        'collumns' => [
-            [
-                'type' => 'dbRow',
-                'name' => 'userName',
-                'htmlInputType' => 'text'
-            ],
-            [
-                'type' => 'dbRow',
-                'name' => 'commentBody',
-                'htmlInputType' => 'text'
-            ],
-            [
-                'type' => 'dbRow',
-                'name' => 'commentParent',
-                'htmlInputType' => 'number'
-            ],
-        ]
-
     ],
 );
 
